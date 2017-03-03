568e6607c5f3b44c32884595d3f50a2cf0d739d1
Max Albrecht <max.albrecht@zhdk.ch>
2017-03-03 17:30:05 +0100

    chore: ui: upgrade video.js to v6
    v6.0.0-RC.3

--- app/assets/javascripts/react/ui-components/VideoJs.js
+++ app/assets/javascripts/react/ui-components/VideoJs.js
@@ -1,8 +1,11 @@
 import React from 'react'
-// import videojs from 'video.js' // FIXME: crashes server, required on mount!
+import videojs from 'video.js'
 import cx from 'classnames'
 import omit from 'lodash/omit'
 import merge from 'lodash/merge'
+/* eslint-disable camelcase */ // props from server
+
+const noop = () => {}
 
 const DEFAULT_OPTIONS = {
   fluid: true,
@@ -29,15 +32,23 @@ const DEFAULT_OPTIONS = {
   }
 }
 
+
+// react wrapper
+
 class VideoJS extends React.Component {
   componentDidMount () {
-    const videoTag = this.refs.video
-    if (!videoTag) throw new Error('no video tag!')
+    if (!this.videoNode) throw new Error('no video tag!')
 
     const playerOptions = merge(DEFAULT_OPTIONS, this.props.options)
     // init:
-    const videojs = require('video.js')
-    this.player = videojs(videoTag, playerOptions)
+    this.player = videojs(this.videoNode, playerOptions, this.props.onPlayerReady)
+  }
+
+  // destroy player on unmount
+  componentWillUnmount () {
+    if (this.player) {
+      this.player.dispose()
+    }
   }
 
   render ({props, state} = this) {
@@ -46,19 +57,21 @@ class VideoJS extends React.Component {
 
     const classes = cx(this.props.className, 'videojs video-js video-fluid vjs-default-skin')
 
-    return (
+    // NOTE: wrap the player in a div with a `data-vjs-player` attribute so videojs won't create additional wrapper in the DOM
+    return (<div data-vjs-player>
       <video ref='video' {...videoProps} className={classes}>
         {sources.map(({url, content_type}) =>
-          <source src={url} type={content_type} key={`${url}${content_type}`}/>
+          <source src={url} type={content_type} key={`${url}${content_type}`} />
         )}
       </video>
-    )
+    </div>)
   }
 }
 
 VideoJS.defaultProps = {
   controls: true,
-  preload: 'auto'
+  preload: 'auto',
+  onPlayerReady: noop
 }
 
 export default VideoJS
