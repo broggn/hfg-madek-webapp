6908c5849259184dd90076f970b385e75aa95815
Max Albrecht <max.albrecht@zhdk.ch>
2018-04-10 20:07:52 +0200

    split layout/view/styles/scripts

--- app/assets/javascripts/react/views/MediaEntry/MediaEntryEmbedded.jsx
+++ app/assets/javascripts/react/views/MediaEntry/MediaEntryEmbedded.jsx
@@ -25,7 +25,7 @@ module.exports = React.createClass({
     if (!media_type == 'video') throw new Error('only videos supported!')
     // const { previews, original_file_url } = media_file
 
-    const hasPlayer = media_type == 'audio' || media_type == 'video'
+    // const hasPlayer = media_type == 'audio' || media_type == 'video'
 
     const defaultSize = {
       width: 500,
@@ -36,21 +36,21 @@ module.exports = React.createClass({
     const eHeight = embed_config.height || defaultSize.height
 
     // for tile body and footer
-    const linkProps = { href: get.url, target: '_blank' }
-    let style = {
-      maxWidth: eWidth > 0 ? eWidth + 'px' : null,
-      maxHeight: eHeight > 0 ? eHeight + 'px' : null,
-      overflow: 'hidden'
-    }
-
-    const bodyStyle = {
-      height: eHeight > 0 ? eHeight - CAPTION_HEIGHT + 'px' : void 0,
-      width: eWidth > 0 ? eWidth + 'px' : void 0,
-      boxShadow: '0 0 150px #575757 inset',
-      display: 'table-cell',
-      verticalAlign: 'middle',
-      textAlign: 'center'
-    }
+    // const linkProps = { href: get.url, target: '_blank' }
+    // let style = {
+    //   maxWidth: eWidth > 0 ? eWidth + 'px' : null,
+    //   maxHeight: eHeight > 0 ? eHeight + 'px' : null,
+    //   overflow: 'hidden'
+    // }
+    //
+    // const bodyStyle = {
+    //   height: eHeight > 0 ? eHeight - CAPTION_HEIGHT + 'px' : void 0,
+    //   width: eWidth > 0 ? eWidth + 'px' : void 0,
+    //   boxShadow: '0 0 150px #575757 inset',
+    //   display: 'table-cell',
+    //   verticalAlign: 'middle',
+    //   textAlign: 'center'
+    // }
 
     // // TODO: choose appropriate sizes (instad of largest)
     // const poster = f
@@ -70,10 +70,10 @@ module.exports = React.createClass({
     switch (media_type) {
       case 'image':
       case 'document':
-        style = f.assign({}, style, {
-          width: eWidth > 0 ? eWidth + 'px' : void 0,
-          height: eHeight > 0 ? eHeight + 'px' : void 0
-        })
+        // style = f.assign({}, style, {
+        //   width: eWidth > 0 ? eWidth + 'px' : void 0,
+        //   height: eHeight > 0 ? eHeight + 'px' : void 0
+        // })
         mediaProps = {
           style: {
             maxWidth: eWidth + 'px',
@@ -96,25 +96,28 @@ module.exports = React.createClass({
         mediaProps = fullsize
     }
 
-    const mediaPreview = <MediaEntryPreview get={get} mediaProps={mediaProps} />
-
-    return (
-      <div style={style}>
-        <div className="ui-tile" style={{ display: 'block' }}>
-          <div className="ui-tile__body" style={bodyStyle}>
-            {hasPlayer ? mediaPreview : <a {...linkProps}>{mediaPreview}</a>}
-          </div>
-          <a className="ui-tile__foot" {...linkProps}>
-            <h3 className="ui-tile__title">{caption_text[0]}</h3>
-            <h4 className="ui-tile__meta">
-              <span>{caption_text[1]}</span>
-            </h4>
-            <span className="ui-tile__flags">
-              <i className="icon-link ui-tile__flag ui-tile__flag--type" />
-            </span>
-          </a>
-        </div>
-      </div>
+    const mediaPreview = (
+      <MediaEntryPreview get={get} mediaProps={mediaProps} captionText={caption_text} />
     )
+    return mediaPreview
+
+    // return (
+    //   <div style={style}>
+    //     <div className="ui-tile" style={{ display: 'block' }}>
+    //       <div className="ui-tile__body" style={bodyStyle}>
+    //         {hasPlayer ? mediaPreview : <a {...linkProps}>{mediaPreview}</a>}
+    //       </div>
+    //       <a className="ui-tile__foot" {...linkProps}>
+    //         <h3 className="ui-tile__title">{caption_text[0]}</h3>
+    //         <h4 className="ui-tile__meta">
+    //           <span>{caption_text[1]}</span>
+    //         </h4>
+    //         <span className="ui-tile__flags">
+    //           <i className="icon-link ui-tile__flag ui-tile__flag--type" />
+    //         </span>
+    //       </a>
+    //     </div>
+    //   </div>
+    // )
   }
 })
