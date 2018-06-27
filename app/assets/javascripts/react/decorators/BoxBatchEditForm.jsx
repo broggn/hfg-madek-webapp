8091ef3a29f365f9f783d36ebbe7aa5e4fc314a5
phiku <philipp.kuederli@zhdk.ch>
2018-06-27 16:09:41 +0200

    show meta keys in bubbles

--- app/assets/javascripts/react/decorators/BoxBatchEditForm.jsx
+++ app/assets/javascripts/react/decorators/BoxBatchEditForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import f from 'active-lodash'
+import l from 'lodash'
 import t from '../../lib/i18n-translate.js'
 import cx from 'classnames/dedupe'
 
@@ -12,6 +12,70 @@ class BoxBatchEditForm extends React.Component {
     super(props)
   }
 
+  allMetaKeyIds() {
+    var metaMetaData = this.props.stateBatch.metaMetaData
+
+
+    return l.uniq(l.flatten(l.map(
+      metaMetaData,
+      (mmd) => l.keys(mmd.data.meta_key_by_meta_key_id)
+    )))
+
+  }
+
+  allMetaKeysById() {
+    var metaMetaData = this.props.stateBatch.metaMetaData
+
+    return l.reduce(
+      metaMetaData,
+      (memo, mmd) => {
+        return l.merge(
+          memo,
+          mmd.data.meta_key_by_meta_key_id
+        )
+      },
+      {}
+    )
+  }
+
+  metaKeysWithTypes() {
+    var metaMetaData = this.props.stateBatch.metaMetaData
+
+    return l.map(
+      this.allMetaKeyIds(),
+      (k) => {
+        return {
+          metaKeyId: k,
+          types: l.map(
+            l.filter(
+              metaMetaData,
+              (mmd) => {
+                return l.has(mmd.data.meta_key_by_meta_key_id, k)
+              }
+            ),
+            (m) => m.type
+          ),
+          metaKey: this.allMetaKeysById()[k]
+        }
+      }
+    )
+  }
+
+  renderKey(k) {
+    return (
+      <div key={k.metaKeyId} style={{display: 'inline-block', backgroundColor: 'white', borderRadius: '5px', padding: '0px 10px', marginRight: '5px', marginBottom: '5px'}}>
+        {k.metaKey.label}
+      </div>
+    )
+  }
+
+  renderKeys() {
+    return l.map(
+      this.metaKeysWithTypes(),
+      (k) => this.renderKey(k)
+    )
+  }
+
   render() {
 
     var stateBatch = this.props.stateBatch
@@ -21,7 +85,7 @@ class BoxBatchEditForm extends React.Component {
     } else {
       return (
         <div className='ui-resources-holder pam'>
-          form
+          {this.renderKeys()}
         </div>
       )
     }
