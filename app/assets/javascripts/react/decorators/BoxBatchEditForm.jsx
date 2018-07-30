83ab0fd74ac02e3a7c791c9945e56ad4fb62754d
phiku <philipp.kuederli@zhdk.ch>
2018-07-30 10:28:20 +0200

    extract meta key selector

--- app/assets/javascripts/react/decorators/BoxBatchEditForm.jsx
+++ app/assets/javascripts/react/decorators/BoxBatchEditForm.jsx
@@ -5,6 +5,7 @@ import t from '../../lib/i18n-translate.js'
 import cx from 'classnames/dedupe'
 import BoxBatchEditMetaKeyForm from './BoxBatchEditMetaKeyForm.jsx'
 import BoxBatchEditFormKeyBubbles from './BoxBatchEditFormKeyBubbles.jsx'
+import BoxMetaKeySelector from './BoxMetaKeySelector.jsx'
 
 class BoxBatchEditForm extends React.Component {
 
@@ -347,122 +348,8 @@ class BoxBatchEditForm extends React.Component {
     )
   }
 
-  findMetaKeysWithTypes(metaKeyIds) {
-    var metaKeysWithTypes = this.stateBatch().components.loadMetaMetaData.data.metaKeysWithTypes
-    return l.map(
-      metaKeyIds,
-      (k) => l.find(metaKeysWithTypes, (mkt) => mkt.metaKeyId == k)
-    )
-  }
 
-  renderMandatories() {
-    var metaKeyIds = l.uniq(
-      l.flatten(
-        l.map(
-          this.stateBatch().components.loadMetaMetaData.data.metaMetaData,
-          (mmd) => l.keys(mmd.data.mandatory_by_meta_key_id)
-        )
-      )
-    )
 
-    return (
-      <div>
-        {'Pflichtfelder:'}
-        {' '}
-        <div
-          style={{
-            display: 'inline-block',
-            marginLeft: '10px',
-            marginBottom: '20px'
-          }}
-        >
-          <BoxBatchEditFormKeyBubbles
-            metaKeysWithTypes={this.findMetaKeysWithTypes(metaKeyIds)}
-            onClickKey={this.props.onClickKey}
-          />
-        </div>
-      </div>
-    )
-  }
-
-  renderVocabularies() {
-
-    var metaMetaDataForTypes = this.stateBox().components.batch.components.loadMetaMetaData.data.metaMetaData
-
-    var vocabularies = l.reduce(
-      metaMetaDataForTypes,
-      (memo, mmd) => {
-        l.each(
-          mmd.data.vocabularies_by_vocabulary_id,
-          (v, k) => memo[k] = v
-        )
-        return memo
-      },
-      {}
-    )
-
-    var vocabMetaKeys = l.reduce(
-      l.map(metaMetaDataForTypes, (mmd) => mmd.data.meta_key_ids_by_vocabulary_id),
-      (memo, vocab2Key) => {
-        l.each(
-          vocab2Key,
-          (v, k) => memo[k] = l.uniq(l.concat((memo[k] ? memo[k] : []), v))
-        )
-        return memo
-      },
-      {}
-    )
-
-    var metaKeysWithTypes = this.stateBatch().components.loadMetaMetaData.data.metaKeysWithTypes
-
-    return l.map(
-      vocabularies,
-      (v, k) => {
-
-        var isSelected = () => {
-          return this.stateBatch().data.selectedVocabulary == k
-        }
-
-        var renderBubbles = () => {
-          if(!isSelected()) {
-            return null
-          }
-          return (
-            <BoxBatchEditFormKeyBubbles
-              metaKeysWithTypes={this.findMetaKeysWithTypes(vocabMetaKeys[k])}
-              onClickKey={this.props.onClickKey}
-            />
-          )
-        }
-
-        return (
-          <div key={k}>
-            <div>
-              <div style={{marginBottom: (isSelected() ? '10px' : '0px')}}>
-                <div>
-                  <span className={(isSelected() ? 'open' : null)} style={{cursor: 'pointer'}} onClick={(e) => this.stateBatch().trigger(this.stateBatch(), {action: 'select-vocabulary', vocabulary: k})}>
-                    <i
-                      className={'ui-side-filter-lvl1-marker'}
-                      style={{
-                        position: 'static',
-                        float: 'left',
-                        width: '20px',
-                        marginTop: '4px'
-                      }}
-                    />
-                    {v.label}
-                  </span>
-                </div>
-                <div style={{marginLeft: '20px', marginTop: '5px'}}>
-                  {renderBubbles()}
-                </div>
-              </div>
-            </div>
-          </div>
-        )
-      }
-    )
-  }
 
   renderInvalidMessage() {
 
@@ -486,9 +373,9 @@ class BoxBatchEditForm extends React.Component {
     } else {
       return (
         <div className='ui-resources-holder pam'>
+
           <div style={{width: '50%', float: 'left'}}>
-            {this.renderMandatories()}
-            {this.renderVocabularies()}
+            <BoxMetaKeySelector stateBox={this.props.stateBox} onClickKey={this.props.onClickKey} />
           </div>
           <div style={{width: '50%', float: 'right'}}>
             {this.renderInvalidMessage()}
