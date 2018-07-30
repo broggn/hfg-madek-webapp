5cb950c7fd71b2fa45a76e7261b0059fdefb5456
phiku <philipp.kuederli@zhdk.ch>
2018-07-30 13:15:46 +0200

    randomized error

--- app/controllers/modules/resources/meta_data_update.rb
+++ app/controllers/modules/resources/meta_data_update.rb
@@ -31,6 +31,8 @@ module Modules
         resource = get_authorized_resource
         errors = update_all_meta_data_transaction!(resource, meta_data_params)
 
+        errors['test'] = 'test' if rand() > 0.8
+
         if errors.empty?
           published_before = published_state(resource)
           if resource.class == MediaEntry
