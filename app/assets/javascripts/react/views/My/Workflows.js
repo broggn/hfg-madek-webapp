8fa9a8b5ddb1e56f8e40aba5359e6a452b685f01
Max Albrecht <max.albrecht@zhdk.ch>
2019-07-23 10:54:25 +0200

    fixup! WIP UI

--- app/assets/javascripts/react/views/My/Workflows.js
+++ app/assets/javascripts/react/views/My/Workflows.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Link from '../../ui-components/Link.cjsx'
 import ResourceThumbnail from '../../decorators/ResourceThumbnail.cjsx'
-// import f from 'lodash'
+import f from 'lodash'
 // import setUrlParams from '../../../lib/set-params-for-url.coffee'
 // import AppRequest from '../../../lib/app-request.coffee'
-// import asyncWhile from 'async/whilst'
 // import { parse as parseUrl } from 'url'
 // import { parse as parseQuery } from 'qs'
 // import Moment from 'moment'
@@ -22,30 +22,47 @@ class MyWorkflows extends React.Component {
 
     return (
       <div className="ui-resources-holder pal">
-        {workflows.map((workflow, i) => (
-          <div key={i}>
-            <div className="ui-resources-header">
-              <h1 className="title-l ui-resources-title">{workflow.name}</h1>
-              <label style={labelStyle} className="phs mls">
-                {workflow.status}
-              </label>
-              <Link href={workflow.edit_url} mods="strong">
-                Edit
-              </Link>
+        {f.map(workflows, (workflow, i) => {
+          return (
+            <div key={i}>
+              <div className="ui-resources-header">
+                <h1 className="title-l ui-resources-title">{workflow.name}</h1>
+                <label style={labelStyle} className="phs mls">
+                  {workflow.status}
+                </label>
+
+                <IfLet editUrl={f.get(workflow, 'actions.edit.url')}>
+                  {editUrl => (
+                    <Link href={editUrl} mods="strong">
+                      Edit
+                    </Link>
+                  )}
+                </IfLet>
+              </div>
+              <ul className="grid ui-resources">
+                {f.map(workflow.associated_collections, (collection, ci) => (
+                  <div key={ci}>
+                    <ResourceThumbnail get={collection} />
+                  </div>
+                ))}
+              </ul>
+              <hr className="separator mbm" />
             </div>
-            <ul className="grid ui-resources">
-              {workflow.collections.map((collection, ci) => (
-                <div key={ci}>
-                  <ResourceThumbnail get={collection} />
-                </div>
-              ))}
-            </ul>
-            <hr className="separator mbm" />
-          </div>
-        ))}
+          )
+        })}
       </div>
     )
   }
 }
 
 module.exports = MyWorkflows
+
+const IfLet = ({ children, ...bindings }) => {
+  const keys = Object.keys(bindings)
+  if (keys.length > 1) throw new TypeError('IfLet requires 0 or 1 bindings!')
+  const binding = bindings[keys[0]]
+  return binding ? children(binding) : null
+}
+IfLet.propTypes = {
+  children: PropTypes.func.isRequired
+}
