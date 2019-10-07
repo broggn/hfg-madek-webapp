7ed7d8da9e66b1c421e4c598c65517dc44cde724
Michał Podlecki <m.podlecki@op.pl>
2019-10-07 13:56:08 +0200

    workflow cannot be finisheded if it's errors

--- app/assets/javascripts/react/views/My/WorkflowPreview.js
+++ app/assets/javascripts/react/views/My/WorkflowPreview.js
@@ -8,24 +8,89 @@ import ResourceThumbnail from '../../decorators/ResourceThumbnail.cjsx'
 import Renderer from '../../decorators/metadataedit/MetadataEditRenderer.cjsx'
 import Link from '../../ui-components/Link.cjsx'
 import RailsForm from '../../lib/forms/rails-form.cjsx'
+import validation from '../../../lib/metadata-edit-validation.coffee'
 
 class WorkflowPreview extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      models: [],
-      errors: [],
-      bundleState: false
+      models: {},
+      errors: {},
+      isFinishing: false
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  _batchConflictByMetaKey() {
+  handleSubmit(e) {
+    this.setState({isFinishing: true})
+    true
+  }
 
+  componentWillMount() {
+    console.log('props', this.props)
+    const { child_resources } = this.props.get
+    const { errors, models } = this.state
+
+    f.each(child_resources, (childResource) => {
+      const { resource, meta_data } = childResource
+
+      f.each(childResource.meta_meta_data.meta_key_by_meta_key_id, (meta_key, meta_key_id) => {
+        const model = {
+          meta_key: meta_key,
+          values: meta_data.meta_datum_by_meta_key_id[meta_key_id].values || []
+        }
+        model.originalValues = model.values
+
+        f.set(models, [resource.uuid, meta_key_id], model)
+
+        const validationModel = {}
+        validationModel[meta_key_id] = f.get(models, [resource.uuid, meta_key_id])
+        if(validation._validityForAll(childResource.meta_meta_data, validationModel) === 'invalid') {
+          if(!f.has(errors, resource.uuid)) {
+            f.set(errors, resource.uuid, [])
+          }
+          errors[resource.uuid].push(meta_key_id)
+        }
+
+        this.setState({ models, errors })
+      })
+    })
+  }
+
+  handleValueChange(values, meta_key_id, childResource) {
+    const { errors, models } = this.state
+    const resource_id = childResource.resource.uuid
+    f.set(models, [resource_id, meta_key_id, 'values'], values)
+
+    const validationModel = {}
+    validationModel[meta_key_id] = f.get(models, [resource_id, meta_key_id])
+    // console.log(validationModel)
+
+    // console.log(validation._validityForAll(childResource.meta_meta_data, validationModel))
+
+    if(f.has(errors, resource_id) && f.isArray(errors[resource_id]) && f.includes(errors[resource_id], meta_key_id)) {
+      f.remove(errors[resource_id], (x) => x === meta_key_id)
+      if(f.isEmpty(errors[resource_id])) {
+        delete errors[resource_id]
+      }
+    }
+
+    if(validation._validityForAll(childResource.meta_meta_data, validationModel) === 'invalid') {
+      if(!f.has(errors, resource_id)) {
+        f.set(errors, resource_id, [])
+      }
+      errors[resource_id].push(meta_key_id)
+    }
+
+    // console.log('errors', errors)
+
+    this.setState({ models, errors })
   }
 
   render() {
     const { get } = this.props
     const supHeadStyle = { textTransform: 'uppercase', fontSize: '85%', letterSpacing: '0.15em' }
+    const submitBtnClass = cx('button primary-button large', { disabled: this.state.isFinishing })
 
     return (
       <section className="ui-container bright bordered rounded mas pam">
@@ -43,7 +108,7 @@ class WorkflowPreview extends React.Component {
         >
 
           {get.child_resources.map((childResource, i) => {
-            console.log('childResource', childResource)
+            // console.log('childResource', childResource)
             const { meta_data, meta_meta_data, workflow, resource, type } = childResource
             const { meta_key_by_meta_key_id, mandatory_by_meta_key_id } = meta_meta_data
 
@@ -59,18 +124,16 @@ class WorkflowPreview extends React.Component {
                       return null
                     }
 
-                    const model = {
-                      meta_key: meta_key,
-                      values: meta_data.meta_datum_by_meta_key_id[meta_key_id].values || []
-                    }
-                    model.originalValues = model.values
-                    const cssClass = cx('ui-form-group prh columned', {'error': f.has(mandatory_by_meta_key_id, meta_key_id)})
+                    const model = f.get(this.state.models, [resource.uuid, meta_key_id])
+                    const hasError = f.has(this.state.errors, resource.uuid) && f.isArray(this.state.errors[resource.uuid]) && f.includes(this.state.errors[resource.uuid], meta_key_id)
+                    console.log('hasError', hasError)
+                    const cssClass = cx('ui-form-group prh columned', {'error': f.has(mandatory_by_meta_key_id, meta_key_id) && hasError})
                     const fieldName = `meta_data[${type}][${resource.uuid}]`
 
                     return (
                       <fieldset className={cssClass} key={meta_key_id}>
                         {Renderer._renderLabelByVocabularies(meta_meta_data, meta_key_id)}
-                        {Renderer._renderValueByContext((values) => console.log(meta_key_id, values), fieldName, null, meta_key, false, model, workflow)}
+                        {Renderer._renderValueByContext((values) => this.handleValueChange(values, meta_key_id, childResource), fieldName, null, meta_key, false, model, workflow)}
                       </fieldset>
                     )
                   }
@@ -85,8 +148,8 @@ class WorkflowPreview extends React.Component {
             <a className='link weak' href={get.actions.edit.url}>
               {'Go back'}
             </a>
-            <button type='submit' className='button primary-button large'>
-              {'Finish…'}
+            <button type='submit' className={submitBtnClass} disabled={!f.isEmpty(this.state.errors)} onClick={this.handleSubmit}>
+              {this.state.isFinishing ? 'Finishing…' : 'Finish'}
             </button>
           </div>
 
--- app/services/workflow_locker/meta_data.rb
+++ app/services/workflow_locker/meta_data.rb
@@ -6,7 +6,8 @@ module WorkflowLocker
       @meta_data.each do |resource_type, ids_with_values|
         ids_with_values.each do |id, meta_keys_with_values|
           meta_keys_with_values.each do |meta_key_id, value|
-            value = sanitize_value(value)
+            value = sanitize_value(value, meta_key_id)
+            next if value.blank?
             resource = find_resource(resource_type, id)
             resource = resource.cast_to_type if resource_type != 'Collection'
             create_meta_datum!(resource, meta_key_id, value, true)
@@ -27,8 +28,13 @@ module WorkflowLocker
       klass.find(id)
     end
 
-    def sanitize_value(value)
-      value.reject(&:blank?)
+    def sanitize_value(value, meta_key_id)
+      meta_key = MetaKey.find(meta_key_id)
+      if %w(MetaDatum::Text MetaDatum::TextDate).include?(meta_key.meta_datum_object_type)
+        value.reject(&:blank?)
+      else
+        value
+      end
     end
   end
 end
