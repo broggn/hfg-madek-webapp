b1a20862cdcf2e690cc3936de281cfcb6ec856ce
Michał Podlecki <m.podlecki@op.pl>
2019-10-08 18:20:53 +0200

    extract fieldset to React component

--- app/assets/javascripts/react/views/My/WorkflowPreview.js
+++ app/assets/javascripts/react/views/My/WorkflowPreview.js
@@ -19,20 +19,22 @@ class WorkflowPreview extends React.Component {
       isFinishing: false
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleValueChange = this.handleValueChange.bind(this)
   }
 
   handleSubmit(e) {
     this.setState({isFinishing: true})
-    true
   }
 
   componentWillMount() {
-    console.log('props', this.props)
     const { child_resources } = this.props.get
     const { errors, models } = this.state
 
     f.each(child_resources, (childResource) => {
       const { resource, meta_data } = childResource
+      const resourceId = childResource.uuid
+
+      errors[childResource.uuid] = []
 
       f.each(childResource.meta_meta_data.meta_key_by_meta_key_id, (meta_key, meta_key_id) => {
         const model = {
@@ -41,15 +43,13 @@ class WorkflowPreview extends React.Component {
         }
         model.originalValues = model.values
 
-        f.set(models, [resource.uuid, meta_key_id], model)
+        f.set(models, [resourceId, meta_key_id], model)
 
-        const validationModel = {}
-        validationModel[meta_key_id] = f.get(models, [resource.uuid, meta_key_id])
-        if(validation._validityForAll(childResource.meta_meta_data, validationModel) === 'invalid') {
-          if(!f.has(errors, resource.uuid)) {
-            f.set(errors, resource.uuid, [])
-          }
-          errors[resource.uuid].push(meta_key_id)
+        const validationModel = {
+          [meta_key_id]: f.get(models, [resourceId, meta_key_id])
+        }
+        if (validation._validityForAll(childResource.meta_meta_data, validationModel) === 'invalid') {
+          errors[resourceId].push(meta_key_id)
         }
 
         this.setState({ models, errors })
@@ -59,32 +59,39 @@ class WorkflowPreview extends React.Component {
 
   handleValueChange(values, meta_key_id, childResource) {
     const { errors, models } = this.state
-    const resource_id = childResource.resource.uuid
-    f.set(models, [resource_id, meta_key_id, 'values'], values)
+    const resourceId = childResource.uuid
+    f.set(models, [resourceId, meta_key_id, 'values'], values)
 
-    const validationModel = {}
-    validationModel[meta_key_id] = f.get(models, [resource_id, meta_key_id])
-    // console.log(validationModel)
-
-    // console.log(validation._validityForAll(childResource.meta_meta_data, validationModel))
-
-    if(f.has(errors, resource_id) && f.isArray(errors[resource_id]) && f.includes(errors[resource_id], meta_key_id)) {
-      f.remove(errors[resource_id], (x) => x === meta_key_id)
-      if(f.isEmpty(errors[resource_id])) {
-        delete errors[resource_id]
-      }
+    if (
+      f.has(errors, resourceId)
+      && f.isArray(errors[resourceId])
+      && f.includes(errors[resourceId], meta_key_id)
+    ) {
+      f.remove(errors[resourceId], (x) => x === meta_key_id)
     }
 
-    if(validation._validityForAll(childResource.meta_meta_data, validationModel) === 'invalid') {
-      if(!f.has(errors, resource_id)) {
-        f.set(errors, resource_id, [])
-      }
-      errors[resource_id].push(meta_key_id)
+    const validationModel = {
+      [meta_key_id]: f.get(models, [resourceId, meta_key_id])
     }
-
-    // console.log('errors', errors)
+    if (validation._validityForAll(childResource.meta_meta_data, validationModel) === 'invalid') {
+      errors[resourceId].push(meta_key_id)
+    }
 
     this.setState({ models, errors })
+  }
+
+  // validate(metaMetaData, resourceId, metaKeyId) {
+  //   const validationModel = {
+  //     [meta_key_id]: f.get(models, [resourceId, meta_key_id])
+  //   }
+
+  //   if (validation._validityForAll(metaMetaData, validationModel) === 'invalid') {
+  //     errors[resourceId].push(meta_key_id)
+  //   }
+  // }
+
+  hasErrors() {
+    return !f.every(f.values(this.state.errors), (arr) => f.isEmpty(arr))
   }
 
   render() {
@@ -107,10 +114,8 @@ class WorkflowPreview extends React.Component {
           authToken={this.props.authToken}
         >
 
-          {get.child_resources.map((childResource, i) => {
-            // console.log('childResource', childResource)
-            const { meta_data, meta_meta_data, workflow, resource, type } = childResource
-            const { meta_key_by_meta_key_id, mandatory_by_meta_key_id } = meta_meta_data
+          {f.map(get.child_resources, (childResource, i) => {
+            const { resource, type, meta_meta_data } = childResource
 
             return (
               <div className='ui-container bordered pal mbs' key={i}>
@@ -119,27 +124,24 @@ class WorkflowPreview extends React.Component {
                   {Renderer._renderThumbnail(resource, false)}
                 </div>
                 <div className='app-body-content table-cell ui-container table-substance ui-container'>
-                  {f.map(meta_key_by_meta_key_id, (meta_key, meta_key_id) => {
-                    if(f.isEmpty(meta_data.meta_datum_by_meta_key_id[meta_key_id].values) && !f.has(mandatory_by_meta_key_id, meta_key_id) && !f.includes(f.map(workflow.common_settings.meta_data, 'meta_key.uuid'), meta_key_id)) {
-                      return null
-                    }
+                  {
+                    f.map(meta_meta_data.meta_key_by_meta_key_id, (meta_key, metaKeyId) => {
+                      const hasError = f.include(this.state.errors[childResource.uuid], metaKeyId)
+                      const model = f.get(this.state.models, [childResource.uuid, metaKeyId])
 
-                    const model = f.get(this.state.models, [resource.uuid, meta_key_id])
-                    const hasError = f.has(this.state.errors, resource.uuid) && f.isArray(this.state.errors[resource.uuid]) && f.includes(this.state.errors[resource.uuid], meta_key_id)
-                    console.log('hasError', hasError)
-                    const cssClass = cx('ui-form-group prh columned', {'error': f.has(mandatory_by_meta_key_id, meta_key_id) && hasError})
-                    const fieldName = `meta_data[${type}][${resource.uuid}]`
-
-                    return (
-                      <fieldset className={cssClass} key={meta_key_id}>
-                        {Renderer._renderLabelByVocabularies(meta_meta_data, meta_key_id)}
-                        {Renderer._renderValueByContext((values) => this.handleValueChange(values, meta_key_id, childResource), fieldName, null, meta_key, false, model, workflow)}
-                      </fieldset>
-                    )
+                      return (
+                        <Fieldset
+                          childResource={childResource}
+                          metaKey={meta_key}
+                          hasError={hasError}
+                          model={model}
+                          handleValueChange={this.handleValueChange}
+                          key={metaKeyId}
+                        />
+                      )
+                    })
                   }
-                  )}
                 </div>
-
               </div>
             )
           })}
@@ -148,7 +150,7 @@ class WorkflowPreview extends React.Component {
             <a className='link weak' href={get.actions.edit.url}>
               {'Go back'}
             </a>
-            <button type='submit' className={submitBtnClass} disabled={!f.isEmpty(this.state.errors)} onClick={this.handleSubmit}>
+            <button type='submit' className={submitBtnClass} disabled={this.hasErrors()} onClick={this.handleSubmit}>
               {this.state.isFinishing ? 'Finishing…' : 'Finish'}
             </button>
           </div>
@@ -160,3 +162,26 @@ class WorkflowPreview extends React.Component {
 }
 
 module.exports = WorkflowPreview
+
+class Fieldset extends React.PureComponent {
+  render() {
+    const { childResource, metaKey, model, hasError, handleValueChange } = this.props
+    const { meta_data, meta_meta_data, resource, type, workflow } = childResource
+    const { mandatory_by_meta_key_id } = meta_meta_data
+    const metaKeyId = metaKey.uuid
+
+    if (f.isEmpty(meta_data.meta_datum_by_meta_key_id[metaKeyId].values) && !f.has(mandatory_by_meta_key_id, metaKeyId) && !f.includes(f.map(workflow.common_settings.meta_data, 'meta_key.uuid'), metaKeyId)) {
+      return null
+    }
+
+    const cssClass = cx('ui-form-group prh columned', {'error': hasError})
+    const fieldName = `meta_data[${type}][${resource.uuid}]`
+
+    return (
+      <fieldset className={cssClass}>
+        {Renderer._renderLabelByVocabularies(meta_meta_data, metaKeyId)}
+        {Renderer._renderValueByContext((values) => handleValueChange(values, metaKeyId, childResource), fieldName, null, metaKey, false, model, workflow)}
+      </fieldset>
+    )
+  }
+}
